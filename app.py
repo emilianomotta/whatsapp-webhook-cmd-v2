ffb41dcb8959546cd6583dd1ea5c2764e4bd5b43
emilianomotta <emilianomotta2025@gmail.com>
2025-06-04 01:55:38 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
 import os
 import pymongo
+import requests
 from datetime import datetime
 
 app = Flask(__name__)
 
 VERIFY_TOKEN = "Emi-token-123"
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://emilianomottadesouza:1XkGVRmrQYtWvHie@cmd-db.esdo09q.mongodb.net/?retryWrites=true&w=majority&appName=cmd-db")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", "EAAKGtLKRBjYBO4ygG1ZAhGZAFHzKkIMdjj0C9awy0ZCKNMZC455Th7epmt6Ac1bbAuNMN6FfqVrhZCgyvhR3ZClHR3ZBbiI2xBO55vWCCDNwEnxe9PHLM01OytWpqTRHpG04UHGyyLHOnwKTTeP5ZBe2eFoKIJJ6rQC6vZBJBCqLltSNX0JZCwEfVQ6fDJ0h3ZCUgAPrDrPVtjTPAFcXfUlckVSJccqkgfKua2JZAYbczFlOojek26g8OwcZD")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://emilianomotta2025:2MurnEOaFb44EIrh@cluster0.oijxuj7.mongodb.net/?retryWrites=true&w=majority&tls=true")
 client = pymongo.MongoClient(MONGO_URI)
 db = client.get_database("cmd-db")
 messages = db.get_collection("messages")
@@ -28,6 +30,28 @@ def webhook():
         data = request.get_json()
         if data:
             messages.insert_one({"data": data, "timestamp": datetime.utcnow()})
+            try:
+                for entry in data.get("entry", []):
+                    for change in entry.get("changes", []):
+                        value = change.get("value", {})
+                        messages_list = value.get("messages", [])
+                        for msg in messages_list:
+                            phone_id = value["metadata"]["phone_number_id"]
+                            from_number = msg["from"]
+                            text = "Mensaje Recibido por el CMD de Montevideo, gracias por informar la salida, saludos."
+                            url = f"https://graph.facebook.com/v19.0/{phone_id}/messages"
+                            headers = {
+                                "Authorization": f"Bearer {ACCESS_TOKEN}",
+                                "Content-Type": "application/json"
+                            }
+                            payload = {
+                                "messaging_product": "whatsapp",
+                                "to": from_number,
+                                "text": {"body": text}
+                            }
+                            requests.post(url, json=payload, headers=headers)
+            except Exception as e:
+                print("Error al responder:", e)
         return "EVENT_RECEIVED", 200
 
 if __name__ == "__main__":
